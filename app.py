ceec22085281a00b66311708c8d9686095fa3a66
Harriet Fisher <harrietfisher@uchicago.edu>
2023-04-10 14:19:26 -0500

    app

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Get the paths of the smaller images
-    smaller_images_path = '/static/smaller_images'
-    smaller_image_files = os.listdir(smaller_images_path)
+    smaller_images_path = 'smaller_images'
+    smaller_image_files = os.listdir('static/' + smaller_images_path)
 
     # Get the path of the larger image
-    larger_image_path = '/static/larger_image.jpg'
+    larger_image_path = 'larger_image.jpg'
 
     # Render the template with the image paths
-    return render_template('/templates/index.html', smaller_images=smaller_image_files, larger_image=larger_image_path)
+    return render_template('index.html', smaller_images=smaller_image_files, larger_image=larger_image_path)
 
 if __name__ == '__main__':
     # Run the Flask app
